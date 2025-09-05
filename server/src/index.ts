751e0b072b73c99f87b86e19b84a19a7f5834b4d
Aritra640 <chatterjeearitra15@gmail.com>
2025-09-06 05:05:56 +0530

    working bun server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,36 @@
-import { Hono } from 'hono'
-import { parseBody } from 'hono/utils/body';
-import { Server } from "socket.io"
+import { Server as BunServer } from "bun";
+import { Hono } from "hono";
+import { Server } from "socket.io";
+import { Server as Engine } from "@socket.io/bun-engine";
+
+const io = new Server();
+const engine = new Engine({ path: "/socket.io/" });
+io.bind(engine);
+
+io.on("connection" , (socket) => {
+  console.log("socket.io connected successfully!")
+})
 
 const app = new Hono();
-const io = new Server();
+app.get("/hello", (c) => c.text("Hello from bun, hono and socket.io"))
 
-app.use(parseBody);
+//Get the websocket handler for bun 
+const { websocket } = engine.handler();
 
-app.get('/hello' , (c) => c.text("Hello World"))
-
-//start the server 
-Bun.serve({
-  fetch(req) {
-    return app.fetch(req)
-  },
+//Export default Bun server config, (no app.listen) bun uses this 
+export default {
   port: 8080,
-});
+  idleTimeout: 30,
 
-io.listen(8081);
+  fetch(req: Request, server: BunServer) {
+    const url = new URL(req.url);
+    if(url.pathname.startsWith("/socket.io")) {
+      //Websocket upgrade and socket.io request go to engine 
+      return engine.handleRequest(req , server);
+    }else {
+      //other http request go to the hono app 
+      return app.fetch(req, server);
+    }
+  },
+  websocket
+}
